643614795670053ea4baff2e108203ab3f66baac
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-10-11 16:26:00 +0700

    Create SpawnCharacter.cs

---
+++ Punerassic Park/Assets/Scripts/SpawnCharacter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject[] Dinosaurs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
