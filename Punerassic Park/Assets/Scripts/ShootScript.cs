727778e659045604ad92374fb020b1ba84750041
pondnarawich <pondnarawich@gmail.com>
2021-10-13 14:48:26 +0700

    uye

--- Punerassic Park/Assets/Scripts/ShootScript.cs
+++ Punerassic Park/Assets/Scripts/ShootScript.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class ShootScript : MonoBehaviour
 {
     public GameObject arCamera;
+    public GameObject dinosaurPrefab;
+    public AudioSource dinoDie;
     
     public void Shoot() {
         RaycastHit hit;
 
         if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit)){
             if(hit.transform.name == "trex(Clone)"){
+                // dinoDie.Play();
                 Destroy(hit.transform.gameObject);
-                
+                float a = Random.Range(-5f, 5f);
+                float b = Random.Range(-5f, 5f);
+                Instantiate(dinosaurPrefab, new Vector3(a, 0f, b), Quaternion.identity);
+                // dinoDie.Stop();
             }
         }
     }
