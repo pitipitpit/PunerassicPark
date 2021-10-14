067b48f965d4a4e4c2227c20571f62ca5d5cb21e
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-10-14 11:56:27 +0700

    update ui for the first level

---
+++ Punerassic Park/Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,131 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void DinosaurScript::Start()
+extern void DinosaurScript_Start_mA4B437D9D9FAF0811E354EDED18A743E55F5D6A0 (void);
+// 0x00000002 System.Void DinosaurScript::Update()
+extern void DinosaurScript_Update_m7E50FB64C027BB34CAC6E1EBADCEA00DAF4895E3 (void);
+// 0x00000003 System.Void DinosaurScript::.ctor()
+extern void DinosaurScript__ctor_m94C2A2CF06B8CB04B11892140ADBC3BEC41BA926 (void);
+// 0x00000004 System.Void ShootScript::Shoot()
+extern void ShootScript_Shoot_mFF81D242F47F7059D0BFFA17D0C824C3649A5602 (void);
+// 0x00000005 System.Collections.IEnumerator ShootScript::playAudio()
+extern void ShootScript_playAudio_m2936BCC6765540FACFD08F819703FD3DF9C7D4AD (void);
+// 0x00000006 System.Void ShootScript::.ctor()
+extern void ShootScript__ctor_m9D99A2EE188B26D935ECED4A5B730EC793BA96C0 (void);
+// 0x00000007 System.Void ShootScript/<playAudio>d__5::.ctor(System.Int32)
+extern void U3CplayAudioU3Ed__5__ctor_m3957B11BB4ED0B85D3B5354A565E0A8E173FC740 (void);
+// 0x00000008 System.Void ShootScript/<playAudio>d__5::System.IDisposable.Dispose()
+extern void U3CplayAudioU3Ed__5_System_IDisposable_Dispose_mCDC695A71B530887FB5E1B0396ED922029F2D39C (void);
+// 0x00000009 System.Boolean ShootScript/<playAudio>d__5::MoveNext()
+extern void U3CplayAudioU3Ed__5_MoveNext_mE8CDBE7E6AF3FA34924963D9337775956251DF87 (void);
+// 0x0000000A System.Object ShootScript/<playAudio>d__5::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CplayAudioU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA18861E77FB8AF73CDDF4256C58C7202AE14462E (void);
+// 0x0000000B System.Void ShootScript/<playAudio>d__5::System.Collections.IEnumerator.Reset()
+extern void U3CplayAudioU3Ed__5_System_Collections_IEnumerator_Reset_m96DE41D5AA690E0D9ADBE52B33280B3C54CD2E64 (void);
+// 0x0000000C System.Object ShootScript/<playAudio>d__5::System.Collections.IEnumerator.get_Current()
+extern void U3CplayAudioU3Ed__5_System_Collections_IEnumerator_get_Current_mAA218790F1748108C5491CC38D2849265640B357 (void);
+// 0x0000000D System.Void SpawnCharacter::Start()
+extern void SpawnCharacter_Start_mE13E2FABD1F363B3E141D201265EA267B2D5F131 (void);
+// 0x0000000E System.Void SpawnCharacter::Update()
+extern void SpawnCharacter_Update_m7608BF660A6A0E78FEFD6D7D474322D948843D51 (void);
+// 0x0000000F System.Collections.IEnumerator SpawnCharacter::StartSpawning()
+extern void SpawnCharacter_StartSpawning_m96AFCEA8E77DC420C98F26D27A467B7410867B1E (void);
+// 0x00000010 System.Void SpawnCharacter::.ctor()
+extern void SpawnCharacter__ctor_mE8926D92BCDAD37CEDCC7ADE9CF9471558B2382C (void);
+// 0x00000011 System.Void SpawnCharacter/<StartSpawning>d__5::.ctor(System.Int32)
+extern void U3CStartSpawningU3Ed__5__ctor_m03D5A0FFD8341EC4D76C2B8E49B29273FEFBCF94 (void);
+// 0x00000012 System.Void SpawnCharacter/<StartSpawning>d__5::System.IDisposable.Dispose()
+extern void U3CStartSpawningU3Ed__5_System_IDisposable_Dispose_m483A1B59295084ACBC0D987F312B02EB57A7991E (void);
+// 0x00000013 System.Boolean SpawnCharacter/<StartSpawning>d__5::MoveNext()
+extern void U3CStartSpawningU3Ed__5_MoveNext_mC3B4DDC3FBEE9CBC03175B7F300B5302DF1E32AC (void);
+// 0x00000014 System.Object SpawnCharacter/<StartSpawning>d__5::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartSpawningU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m764C6A3FB5C4C771DB2F9D2A5E4DE05EE338E21C (void);
+// 0x00000015 System.Void SpawnCharacter/<StartSpawning>d__5::System.Collections.IEnumerator.Reset()
+extern void U3CStartSpawningU3Ed__5_System_Collections_IEnumerator_Reset_m9BFD6B2D5D51B2C3E133F48BBD3E2FC128C004C6 (void);
+// 0x00000016 System.Object SpawnCharacter/<StartSpawning>d__5::System.Collections.IEnumerator.get_Current()
+extern void U3CStartSpawningU3Ed__5_System_Collections_IEnumerator_get_Current_m90D6E38C1A9B024D31BF2A3F9032BA92C4902160 (void);
+static Il2CppMethodPointer s_methodPointers[22] = 
+{
+	DinosaurScript_Start_mA4B437D9D9FAF0811E354EDED18A743E55F5D6A0,
+	DinosaurScript_Update_m7E50FB64C027BB34CAC6E1EBADCEA00DAF4895E3,
+	DinosaurScript__ctor_m94C2A2CF06B8CB04B11892140ADBC3BEC41BA926,
+	ShootScript_Shoot_mFF81D242F47F7059D0BFFA17D0C824C3649A5602,
+	ShootScript_playAudio_m2936BCC6765540FACFD08F819703FD3DF9C7D4AD,
+	ShootScript__ctor_m9D99A2EE188B26D935ECED4A5B730EC793BA96C0,
+	U3CplayAudioU3Ed__5__ctor_m3957B11BB4ED0B85D3B5354A565E0A8E173FC740,
+	U3CplayAudioU3Ed__5_System_IDisposable_Dispose_mCDC695A71B530887FB5E1B0396ED922029F2D39C,
+	U3CplayAudioU3Ed__5_MoveNext_mE8CDBE7E6AF3FA34924963D9337775956251DF87,
+	U3CplayAudioU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA18861E77FB8AF73CDDF4256C58C7202AE14462E,
+	U3CplayAudioU3Ed__5_System_Collections_IEnumerator_Reset_m96DE41D5AA690E0D9ADBE52B33280B3C54CD2E64,
+	U3CplayAudioU3Ed__5_System_Collections_IEnumerator_get_Current_mAA218790F1748108C5491CC38D2849265640B357,
+	SpawnCharacter_Start_mE13E2FABD1F363B3E141D201265EA267B2D5F131,
+	SpawnCharacter_Update_m7608BF660A6A0E78FEFD6D7D474322D948843D51,
+	SpawnCharacter_StartSpawning_m96AFCEA8E77DC420C98F26D27A467B7410867B1E,
+	SpawnCharacter__ctor_mE8926D92BCDAD37CEDCC7ADE9CF9471558B2382C,
+	U3CStartSpawningU3Ed__5__ctor_m03D5A0FFD8341EC4D76C2B8E49B29273FEFBCF94,
+	U3CStartSpawningU3Ed__5_System_IDisposable_Dispose_m483A1B59295084ACBC0D987F312B02EB57A7991E,
+	U3CStartSpawningU3Ed__5_MoveNext_mC3B4DDC3FBEE9CBC03175B7F300B5302DF1E32AC,
+	U3CStartSpawningU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m764C6A3FB5C4C771DB2F9D2A5E4DE05EE338E21C,
+	U3CStartSpawningU3Ed__5_System_Collections_IEnumerator_Reset_m9BFD6B2D5D51B2C3E133F48BBD3E2FC128C004C6,
+	U3CStartSpawningU3Ed__5_System_Collections_IEnumerator_get_Current_m90D6E38C1A9B024D31BF2A3F9032BA92C4902160,
+};
+static const int32_t s_InvokerIndices[22] = 
+{
+	2439,
+	2439,
+	2439,
+	2439,
+	2391,
+	2439,
+	1985,
+	2439,
+	2414,
+	2391,
+	2439,
+	2391,
+	2439,
+	2439,
+	2391,
+	2439,
+	1985,
+	2439,
+	2414,
+	2391,
+	2439,
+	2391,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	22,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
